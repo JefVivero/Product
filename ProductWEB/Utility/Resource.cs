ce6f95b01cfdcccf24139e87e02a0fd08387a444
JefVivero <jefersonalonsov@gmail.com>
2022-05-07 13:30:55 -0500

    Create Dynamic Repository and Product Controller

---
+++ ProductWEB/Utility/Resource.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductWEB.Utility
+{
+    public class Resource
+    {
+        public const string APIBaseURL = "https://localhost:44316/";
+        public const string ProductAPIURL = APIBaseURL + "api/product/";
+        public const string ContentType = "application/json";
+        
+    }
+}
