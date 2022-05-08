e7b5eec3642821605060f677c7e331c4b6b92c9e
JefVivero <jefersonalonsov@gmail.com>
2022-05-07 22:26:31 -0500

    Product Index View and Create Method in ProductController

--- ProductWEB/Controllers/ProductController.cs
+++ ProductWEB/Controllers/ProductController.cs
@@ -4,6 +4,7 @@ using ProductWEB.Repository.IRepository;
 using ProductWEB.Utility;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -24,6 +25,45 @@ namespace ProductWEB.Controllers
         {
 
             return View(await util.GetAllAsync(Resource.ProductAPIURL));
+        }
+
+        public IActionResult Create()
+        {
+            return View(new Product());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                var files = HttpContext.Request.Form.Files;
+                if (files.Count>0)
+                {
+                    byte[] imgBytes = null;
+                    using (Stream stream= files[0].OpenReadStream())
+                    {
+                        using (MemoryStream memoryStream = new MemoryStream())
+                        {
+                            await stream.CopyToAsync(memoryStream);
+                            imgBytes = memoryStream.ToArray();
+                        }
+                    }
+                    product.Image = imgBytes;
+                }
+                var modelStateError= await util.CreateAsync(Resource.ProductAPIURL, product);
+                if (modelStateError.Response.Errors.Count>0)
+                {
+                    foreach (var item in modelStateError.Response.Errors)
+                    {
+                        product.Errors.Add(item);
+                    }
+                    return View(product);
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(product);
         }
     }
 }
